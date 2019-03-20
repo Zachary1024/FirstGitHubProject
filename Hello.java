b5eb3befefa5c1f402bbcb048e70e5b4cba2e44f
Zachary1024 <45141184+Zachary1024@users.noreply.github.com>
2019-03-20 15:11:50 +0800

    HelloGit

---
+++ Hello.java
@@ -0,0 +1,27 @@
+//public表示公开的
+//class表示一个类
+//Hello表示一个类名
+public class Hello{ //定义一个公开的类，起名为Hello
+    //类体
+	//类体中不允许直接编写Java语句，除声明变量之外
+	/*
+	  public表示公开的
+	  static表示静态的
+	  void表示空
+	  main表示方法名
+	  （String[] args）是一个main方法的形式参数列表
+
+	  注意的是：以下的方法是一个程序的主方法，是程序的执行入口
+	  */ 
+	 public static void main(String[] args){ //定义一个公开的静态的主方法
+		 //方法体
+
+		 //Java语句【Java语句以“；”结尾】
+		 //记住：以下这样的代码的作用是向控制台输出一串字符串
+		 //体内的符号都是半角的符号
+		 System.out.println("Hello 111!");
+		 System.out.println("你好，Java，我是张鼎");
+
+}
+   
+}
